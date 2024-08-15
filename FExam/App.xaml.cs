ec036e82ece7eef5a5cffdadc291a73988294f60
krishsheridan <krishpatel0430@gmail.com>
2024-08-15 14:04:50 -0400

    Add project files.

---
+++ FExam/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace FExam
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ FExam/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace FExam
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
